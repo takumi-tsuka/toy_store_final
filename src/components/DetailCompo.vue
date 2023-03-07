0eb180eff5c85274d652a0bb1b8684838deeb7f4
t.iitsuka <takumi99701@gmail.com>
2023-03-06 21:48:00 -0800

    main:common file added

---
+++ src/components/DetailCompo.vue
@@ -0,0 +1,8 @@
+<template>
+    
+</template>
+<script>
+export default {
+    name:"DetailCompo"
+}
+</script>
---
+++ src/components/Home/ProductList.vue
@@ -0,0 +1,8 @@
+<template>
+    
+</template>
+<script>
+export default {
+    name:"ProductCompo"
+}
+</script>
